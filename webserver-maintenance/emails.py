7c738ca5774c8321e80c12f5387002afa7c42a08
Brandon T <brandon025@gmail.com>
2021-08-23 07:19:50 +0000

    Adding health checks for our web server

--- webserver-maintenance/emails.py
+++ webserver-maintenance/emails.py
@@ -32,3 +32,12 @@ def send_email(message):
   mail_server = smtplib.SMTP('localhost')
   mail_server.send_message(message)
   mail_server.quit()
+
+def generate_error_report(sender, recepient, subject, body):
+  message = email.message.EmailMessage()
+  message["From"] = sender
+  message["To"] = recepient
+  message["Subject"] = subject
+  message.set_content(body)
+
+  return message
--- webserver-maintenance/health_check.py
+++ webserver-maintenance/health_check.py
@@ -1,34 +1,53 @@
 #!/usr/bin/env python3
 import psutil
 import shutil
+import socket
+import os
+from emails import generate_error_report, send_email
 
 # Check CPU usage
-def check_cpu_usage:
+def check_cpu_usage():
     cpu_count = psutil.cpu_percent(interval=1)
-    return usage < 80
+    return cpu_count > 80
 
 # Check if theres enough free space on a disk
 def check_disk_usage(disk):
-    du = shutil.disk_uage(disk)
+    du = shutil.disk_usage(disk)
     free = du.free / du.total * 100
     return free < 20
 
 # Check available memory in bytes
 def check_available_mem():
     mem_available = psutil.virtual_memory().available/(1024*1024)
-    return available_memory < 500
+    return mem_available < 500
 
 # Check if local host is correctly configured for 127.0.0.1
+def check_localhost():
     localhost = socket.gethostbyname('localhost')
     return localhost != '127.0.0.1'
 
+# Set error message
 if check_cpu_usage():
     errormsg = "CPU usage is over 80%"
-elif check_disk_uage('/'):
+elif check_disk_usage('/'):
     errormsg = "Available disk space is less than 20%"
 elif check_available_mem():
     errormsg = "Available memory is less than 500MB"
 elif check_localhost():
-    error_message = "localhost cannot resolve to 127.0.0.1"
+    errormsg = "localhost cannot resolve to 127.0.0.1"
 else:
-    pass
+    errormsg = None
+
+# Send email if error message
+if errormsg:
+    print("Maintenance error: " + errormsg)
+    try:
+        sender = "automation@example.com"
+        receiver = "student-03-60bbf41681c1@example.com"
+        #receiver = "{}@example.com".format(os.environ.get('USER'))
+        subject = "Error - {}".format(errormsg)
+        body = "Please check your system and resolve the issue as soon as possible"
+        message = generate_error_report(sender, receiver, subject, body)
+        send_email(message)
+    except NameError:
+        pass
